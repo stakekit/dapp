1143f90a32b46a29aee009e42201134222bdcddc
petar-omni <117348982+petar-omni@users.noreply.github.com>
2023-12-05 10:28:53 +0100

    fix(): mobile layout (#69)

--- src/app/style.css.ts
+++ src/app/style.css.ts
@@ -1,5 +1,6 @@
 import { style } from "@vanilla-extract/css"
 import { atoms } from "../styles"
+import { minMediaQuery } from "../styles/tokens/breakpoints"
 
 export const container = style({
   height: "100%",
@@ -48,9 +49,27 @@ export const widgetContainer = style({ zIndex: 1 })
 export const connectContainer = style({
   borderRadius: "110px",
   background: "rgba(255, 255, 255, 0.05)",
-  padding: "15px 25px",
+  padding: "15px 20px",
+  "@media": {
+    [minMediaQuery("tablet")]: {
+      padding: "15px 25px",
+    },
+  },
 })
 
 export const dividerContainer = style({
   alignSelf: "stretch",
+})
+
+export const logo = style({
+  height: "60px",
+  width: "140px",
+  aspectRatio: "auto 140 / 60",
+  "@media": {
+    [minMediaQuery("tablet")]: {
+      height: "80px",
+      width: "160px",
+      aspectRatio: "auto 160 / 80",
+    },
+  },
 })
