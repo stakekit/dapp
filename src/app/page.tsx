35bf186ac75b0d463a6cd078d5cdaa59cf7ccafc
petar-omni <117348982+petar-omni@users.noreply.github.com>
2024-02-06 18:28:25 +0100

    feat(): bump widget (#101)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import React from "react"
-import { container, widgetContainer } from "./style.css"
-import { Box } from "../components/atoms/box"
 import { Header } from "./header"
 import { Widget } from "./widget"
 import { HelpModals } from "./widget/help-modals"
@@ -8,13 +6,9 @@ import { HelpModals } from "./widget/help-modals"
 export default function Home() {
   return (
     <>
-      <Box className={container}>
-        <Box className={widgetContainer}>
-          <Header />
-          <Widget />
-          <HelpModals />
-        </Box>
-      </Box>
+      <Header />
+      <Widget />
+      <HelpModals />
     </>
   )
 }
--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,25 +1,47 @@
 import { globalStyle } from "@vanilla-extract/css"
-import { widgetContainer } from "../app/style.css"
+
+globalStyle("html", {
+  overflowX: "hidden",
+})
 
 globalStyle("html, body", {
   margin: 0,
+})
+
+globalStyle("body::before", {
+  content: "",
+  position: "absolute",
+  top: "400px",
+  bottom: 0,
+  left: 0,
+  right: 0,
+  width: "100%",
+  height: "35vh",
+  background:
+    "linear-gradient(331deg, #FFC21B 5.21%, #B1A5B9 23.44%, #F46FC8 38.54%, #FF5F25 79.69%, #6D05F3 100%)",
+  filter: "blur(140px)",
+  transform: "rotate(-15.84deg) translate3d(0, 0, 0) translateZ(0)",
 })
 
 globalStyle("body", {
   background: "rgba(18, 20, 21, 1)",
   fontFamily: "var(--sk-font-garamond)",
   minHeight: "100vh",
+
+  display: "flex",
+  flexDirection: "column",
+  position: "relative",
+  overflowX: "hidden",
 })
 
 globalStyle("a", {
   textDecoration: "none",
 })
 
-globalStyle(`${widgetContainer} > [data-rk='stakekit']`, {
+globalStyle("[data-rk='stakekit']", {
   padding: "0 8px",
 })
 
-globalStyle(
-  `${widgetContainer} > [data-rk='stakekit'] > div:first-of-type > div:first-of-type`,
-  { borderRadius: "20px" }
-)
+globalStyle("[data-rk='stakekit'] > div:first-of-type > div:first-of-type", {
+  borderRadius: "20px",
+})
