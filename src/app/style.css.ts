dcfc3c7d33aa5395444027e87993b3ea0f76fa29
Petar Todorovic <petar@omni.app>
2024-02-07 12:46:05 +0100

    feat(): bump widget

--- src/app/style.css.ts
+++ src/app/style.css.ts
@@ -2,15 +2,30 @@ import { style } from "@vanilla-extract/css"
 import { atoms } from "../styles"
 import { minMediaQuery } from "../styles/tokens/breakpoints"
 
-export const layoutContainer = style([
-  atoms({ fontFamily: "body" }),
-  {
-    scrollbarWidth: "none",
-    "::-webkit-scrollbar": {
-      display: "none",
-    },
+export const layoutContainer = style([atoms({ fontFamily: "body" })])
+
+export const container = style({
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  position: "relative",
+  overflow: "hidden",
+
+  "::before": {
+    content: "",
+    position: "absolute",
+    top: "400px",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: "100%",
+    height: "35vh",
+    background:
+      "linear-gradient(331deg, #FFC21B 5.21%, #B1A5B9 23.44%, #F46FC8 38.54%, #FF5F25 79.69%, #6D05F3 100%)",
+    filter: "blur(140px)",
+    transform: "rotate(-15.84deg) translate3d(0, 0, 0) translateZ(0)",
   },
-])
+})
 
 export const links = style({
   cursor: "pointer",
