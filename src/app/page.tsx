d562b1105ab64da5d1abc7472460b69c937a94e9
petar-omni <117348982+petar-omni@users.noreply.github.com>
2024-02-07 12:53:53 +0100

    feat(): bump widget (#102)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import React from "react"
 import { Header } from "./header"
 import { Widget } from "./widget"
 import { HelpModals } from "./widget/help-modals"
+import { Box } from "../components/atoms/box"
+import { container } from "./style.css"
 
 export default function Home() {
   return (
-    <>
+    <Box className={container}>
       <Header />
       <Widget />
       <HelpModals />
-    </>
+    </Box>
   )
 }
--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,37 +1,14 @@
 import { globalStyle } from "@vanilla-extract/css"
-
-globalStyle("html", {
-  overflowX: "hidden",
-})
+import { maxMediaQuery } from "./tokens/breakpoints"
 
 globalStyle("html, body", {
   margin: 0,
-})
-
-globalStyle("body::before", {
-  content: "",
-  position: "absolute",
-  top: "400px",
-  bottom: 0,
-  left: 0,
-  right: 0,
-  width: "100%",
-  height: "35vh",
-  background:
-    "linear-gradient(331deg, #FFC21B 5.21%, #B1A5B9 23.44%, #F46FC8 38.54%, #FF5F25 79.69%, #6D05F3 100%)",
-  filter: "blur(140px)",
-  transform: "rotate(-15.84deg) translate3d(0, 0, 0) translateZ(0)",
 })
 
 globalStyle("body", {
   background: "rgba(18, 20, 21, 1)",
   fontFamily: "var(--sk-font-garamond)",
   minHeight: "100vh",
-
-  display: "flex",
-  flexDirection: "column",
-  position: "relative",
-  overflowX: "hidden",
 })
 
 globalStyle("a", {
@@ -39,7 +16,11 @@ globalStyle("a", {
 })
 
 globalStyle("[data-rk='stakekit']", {
-  padding: "0 8px",
+  "@media": {
+    [maxMediaQuery("tablet")]: {
+      padding: "0 8px",
+    },
+  },
 })
 
 globalStyle("[data-rk='stakekit'] > div:first-of-type > div:first-of-type", {
