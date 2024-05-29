4fd67323102ee768138b2d796f15fe8989953fbb
petar-omni <117348982+petar-omni@users.noreply.github.com>
2024-05-29 14:52:30 +0200

    feat(): bump widget (#161)

--- src/app/tracking.ts
+++ src/app/tracking.ts
@@ -1,11 +1,21 @@
 import { SKAppProps } from "@stakekit/widget"
 import mixpanel from "mixpanel-browser"
+import { config } from "../config"
 
-export const tracking: SKAppProps["tracking"] = {
-  trackEvent: (...args) => {
-    mixpanel.track(...args)
-  },
-  trackPageView: (page, props) => {
-    mixpanel.track_pageview({ page, ...(props && { props }) })
-  },
+export const mixpanelEnabled =
+  typeof window !== "undefined" && config.mixPanelToken
+
+if (mixpanelEnabled) {
+  mixpanel.init(config.mixPanelToken)
 }
+
+export const tracking: SKAppProps["tracking"] = mixpanelEnabled
+  ? {
+      trackEvent: (...args) => {
+        mixpanel.track(...args)
+      },
+      trackPageView: (page, props) => {
+        mixpanel.track_pageview({ page, ...(props && { props }) })
+      },
+    }
+  : undefined
