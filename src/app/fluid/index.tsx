a0c4f812f66799a777b5a56fefced865b1e15213
Stefan Parezanovic <33752425+Stefanats@users.noreply.github.com>
2024-05-29 17:08:23 +0200

    feat(): hide fluid animation on mobile

--- src/app/fluid/index.tsx
+++ src/app/fluid/index.tsx
@@ -3,10 +3,12 @@
 import { useEffect, useRef } from "react"
 import { startFluid } from "@stakekit/fluid-animation"
 import { styles } from "./styles.css"
+import { useIsMobile } from "@/utils/use-is-mobile"
 
 export const FluidContainer = () => {
   const fluidStarted = useRef(false)
   const canvasContainerRef = useRef<HTMLDivElement>(null)
+  const isMobile = useIsMobile()
 
   useEffect(() => {
     if (!canvasContainerRef.current || fluidStarted.current) return
@@ -15,5 +17,6 @@ export const FluidContainer = () => {
     startFluid(canvasContainerRef.current)
   }, [])
 
+  if (isMobile) return null
   return <div ref={canvasContainerRef} className={styles} />
 }
