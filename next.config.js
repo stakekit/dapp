f9c51595a22385fef582174c7ad14b8e3a8c37e0
petar-omni <117348982+petar-omni@users.noreply.github.com>
2025-12-04 13:04:33 +0100

    chore: bump deps (#237)

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.optimization.splitChunks = false;
-    config.module.rules.push({
-      resourceQuery: /raw/,
-      type: "asset/source",
-    });
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default withVanillaExtract(nextConfig);
