3a4fedc96a5b7b0292812f0ec2aa24b992a3fed0
Petar Todorovic <petar@stakek.it>
2026-09-04 15:37:27 +0200

    feat: bump widget

--- src/app/widget/help-modals.tsx
+++ src/app/widget/help-modals.tsx
@@ -1,68 +1,65 @@
 "use client";
 
-import { HelpModal, TrackingContextProvider } from "@stakekit/widget";
+import { HelpModal } from "@stakekit/widget";
 import { Box } from "../../components/atoms/box";
 import { Text } from "../../components/atoms/typography";
 import { links } from "../style.css";
-import { tracking } from "../tracking";
 
 export const HelpModals = () => (
-  <TrackingContextProvider tracking={tracking}>
+  <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    flexDirection="column"
+    gap="8"
+    marginBottom="14"
+  >
+    <HelpModal
+      modal={{ type: "whatIsStakeKit" }}
+      customTrigger={
+        <Box className={links}>
+          <Text variant={{ size: "supportLink" }}>What is Yield.xyz?</Text>
+        </Box>
+      }
+    />
+
+    <HelpModal
+      modal={{ type: "getInTouch" }}
+      customTrigger={
+        <Box className={links}>
+          <Text variant={{ size: "supportLink" }}>
+            Need help or have questions?
+          </Text>
+        </Box>
+      }
+    />
+
+    <Box as="a" href="https://twitter.com/yield_xyz" target="_blank">
+      <Text variant={{ size: "supportLink" }}>Follow us on Twitter</Text>
+    </Box>
+
     <Box
       display="flex"
       justifyContent="center"
       alignItems="center"
-      flexDirection="column"
       gap="8"
-      marginBottom="14"
+      marginTop="8"
     >
-      <HelpModal
-        modal={{ type: "whatIsStakeKit" }}
-        customTrigger={
-          <Box className={links}>
-            <Text variant={{ size: "supportLink" }}>What is Yield.xyz?</Text>
-          </Box>
-        }
-      />
-
-      <HelpModal
-        modal={{ type: "getInTouch" }}
-        customTrigger={
-          <Box className={links}>
-            <Text variant={{ size: "supportLink" }}>
-              Need help or have questions?
-            </Text>
-          </Box>
-        }
-      />
-
-      <Box as="a" href="https://twitter.com/yield_xyz" target="_blank">
-        <Text variant={{ size: "supportLink" }}>Follow us on Twitter</Text>
+      <Box
+        as="a"
+        href="https://docs.yield.xyz/docs/terms-of-use"
+        target="_blank"
+      >
+        <Text>Terms & Conditions</Text>
       </Box>
 
       <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        gap="8"
-        marginTop="8"
+        as="a"
+        href="https://docs.yield.xyz/docs/privacy-policy"
+        target="_blank"
       >
-        <Box
-          as="a"
-          href="https://docs.yield.xyz/docs/terms-of-use"
-          target="_blank"
-        >
-          <Text>Terms & Conditions</Text>
-        </Box>
-
-        <Box
-          as="a"
-          href="https://docs.yield.xyz/docs/privacy-policy"
-          target="_blank"
-        >
-          <Text>Privacy Policy</Text>
-        </Box>
+        <Text>Privacy Policy</Text>
       </Box>
     </Box>
-  </TrackingContextProvider>
+  </Box>
 );
