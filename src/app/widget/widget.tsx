c83542a540341a1336bb0f13165e7be01101a6c5
petar-omni <117348982+petar-omni@users.noreply.github.com>
2026-09-04 17:03:13 +0200

    feat: bump widget (#241)

--- src/app/widget/widget.tsx
+++ src/app/widget/widget.tsx
@@ -8,55 +8,41 @@ import { tracking } from "../tracking";
 import { HelpModals } from "./help-modals";
 
 export const Widget = () => {
+  const theme = {
+    ...darkTheme,
+    color: {
+      ...(darkTheme.color ?? {}),
+      background: "rgba(0, 0, 0, 0.83)",
+      modalBodyBackground: "#121415",
+      selectValidatorMultiDefaultBackground: "#121415",
+      positionsClaimRewardsBackground: "#121415",
+      positionsActionRequiredBackground: "#121415",
+      positionsPendingBackground: "#121415",
+      connectKit: {
+        ...(darkTheme.color?.connectKit ?? {}),
+        modalBackground: "#121415",
+        profileForeground: "#121415",
+      },
+      secondaryButtonBackground: "#FFFFFF0D",
+      secondaryButtonColor: "#EEF0F2",
+      secondaryButtonActiveBackground: "#ffffff1a",
+      secondaryButtonHoverBackground: "#ffffff1a",
+      stakeSectionBackground: "#FFFFFF0D",
+      backgroundMuted: "#FFFFFF0D",
+      tokenSelectBackground: "#FFFFFF0D",
+      tokenSelectHoverBackground: "#ffffff1a",
+      skeletonLoaderBase: "#FFFFFF0D",
+      skeletonLoaderHighlight: "#2B2B2B",
+      dropdownBackground: "#121415",
+      warningBoxBackground: "#FFFFFF0D",
+    },
+  };
+
   return (
-    <>
-      <SKApp
-        apiKey={config.apiKey}
-        theme={{
-          ...darkTheme,
-          color: {
-            ...darkTheme.color,
-            background: "rgba(0, 0, 0, 0.83)",
-            modalBodyBackground: "#121415",
-            selectValidatorMultiDefaultBackground: "#121415",
-            positionsSectionBackgroundColor: "#121415",
-            positionsSectionBorderColor: "#121415",
-
-            connectKit: {
-              ...darkTheme.color.connectKit,
-              modalBackground: "#121415",
-              profileForeground: "#121415",
-            },
-
-            secondaryButtonBackground: "#FFFFFF0D",
-            secondaryButtonOutline: "#FFFFFF0D",
-
-            secondaryButtonActiveBackground: "#ffffff1a",
-            secondaryButtonActiveOutline: "#ffffff1a",
-            secondaryButtonActiveColor: "#EEF0F2",
-
-            secondaryButtonHoverBackground: "#ffffff1a",
-            secondaryButtonHoverOutline: "#ffffff1a",
-            secondaryButtonHoverColor: "#EEF0F2",
-
-            stakeSectionBackground: "#FFFFFF0D",
-            backgroundMuted: "#FFFFFF0D",
-            tokenSelectBackground: "#FFFFFF0D",
-            positionsSectionDividerColor: "#FFFFFF0D",
-            tokenSelectHoverBackground: "#ffffff1a",
-
-            skeletonLoaderBase: "#FFFFFF0D",
-            skeletonLoaderHighlight: "#2B2B2B",
-            dropdownBackground: "#121415",
-            warningBoxBackground: "#FFFFFF0D",
-          },
-        }}
-        tracking={tracking}
-      />
-
+    <SKApp apiKey={config.apiKey} theme={theme} tracking={tracking}>
       <Box marginTop={{ tablet: "0", mobile: "8" }}>
         <HelpModals />
       </Box>
-    </>
+    </SKApp>
   );
 };
